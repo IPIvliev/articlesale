1196603183ebf6612fb0bb7b67f4ca93b0418369
IPIvliev <cccp1945@mail.ru>
2014-04-23 17:17:46 +0400

    Precomplite

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,6 @@ module Articlesale
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Europe/Moscow'
 
-    config.assets.precompile += ['index.css']
-
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
